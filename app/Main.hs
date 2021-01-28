d08ab88758809e27cc2cab7f998693e9db647983
pe200012 <1326263755@qq.com>
2021-01-28 15:21:55 +0800

    augment function

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,14 @@
 
 module Main where
 
-import           Data.Array                     ( Array )
+import           Data.Array                     ( bounds
+                                                , (!)
+                                                , indices
+                                                , listArray
+                                                , Array
+                                                )
+import           Data.Foldable                  ( for_ )
+import           Control.Monad                  ( when )
 
 newtype Grid = Grid
   { unGrid :: Array (Int, Int) Int
@@ -13,6 +20,15 @@ data Direction
   | D
   | L
   | R
+
+emptyGrid :: Grid
+emptyGrid = Grid (listArray ((0, 0), (3, 3)) (repeat 0))
+
+display :: Grid -> IO ()
+display (Grid arr) = for_ (indices arr) $ \i -> do
+    putStr (show (arr ! i))
+    when (snd i == cB) (putChar '\n')
+    where cB = snd $ snd (bounds arr)
 
 main :: IO ()
 main = pure ()
