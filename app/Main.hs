82abebcb017eb2308686d578aefc7941ffd4a3f7
pe200012 <1326263755@qq.com>
2021-01-28 18:17:14 +0800

    add graphic functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main where
 
-import           Data.Array                     ( (//)
+import           Data.Array                     ( assocs
+                                                , (//)
                                                 , bounds
                                                 , (!)
                                                 , indices
@@ -12,11 +14,35 @@ import           Data.Array                     ( (//)
                                                 )
 import           Data.Foldable                  ( for_ )
 import           Control.Monad                  ( when )
-import           System.Random                  ( getStdGen
+import           System.Random                  ( StdGen
+                                                , getStdGen
                                                 , RandomGen
                                                 , randomR
                                                 )
-import           Control.Arrow                  ( Arrow(first) )
+import           Control.Arrow                  ( ArrowApply(app)
+                                                , Arrow(first)
+                                                )
+import           Graphics.Gloss                 ( play
+                                                , text
+                                                , translate
+                                                , white
+                                                , line
+                                                , pictures
+                                                , display
+                                                , Display(InWindow)
+                                                , Picture
+                                                )
+import           Data.Bifunctor                 ( Bifunctor(bimap, second) )
+import           Graphics.Gloss.Interface.IO.Game
+                                                ( Event(EventKey)
+                                                , Key(Char)
+                                                , KeyState(Down)
+                                                )
+import           System.Exit                    ( exitSuccess )
+import           Control.Exception              ( catch
+                                                , throw
+                                                , Exception
+                                                )
 
 newtype Grid = Grid
   { unGrid :: Array (Int, Int) Int
@@ -32,8 +58,8 @@ data Direction
 emptyGrid :: Grid
 emptyGrid = Grid (listArray ((0, 0), (3, 3)) (repeat 0))
 
-display :: Grid -> IO ()
-display (Grid arr) = for_ (indices arr) $ \i -> do
+displayGrid :: Grid -> IO ()
+displayGrid (Grid arr) = for_ (indices arr) $ \i -> do
     putStr (show (arr ! i))
     putChar $ if snd i == cB then '\n' else ' '
     where cB = snd $ snd (bounds arr)
@@ -83,7 +109,7 @@ generateNum origin@(Grid arr, g) = if null emptySlots then origin else (Grid (ar
 newGame :: IO ()
 newGame = do
     (g, gen) <- generateNum . (emptyGrid, ) <$> getStdGen
-    display g
+    displayGrid g
     final <- gameLoop gen g
     print "Game Over"
     putStrLn ("Score: " ++ show (score final))
@@ -99,12 +125,61 @@ newGame = do
                     Just d ->
                         let grid'          = grid `move` d
                             (grid'', gen') = generateNum (grid', gen)
-                        in  if grid' == grid then gameLoop gen grid else display grid'' >> gameLoop gen' grid''
+                        in  if grid' == grid then gameLoop gen grid else displayGrid grid'' >> gameLoop gen' grid''
     keyDispatch 'w' = return U
     keyDispatch 's' = return D
     keyDispatch 'a' = return L
     keyDispatch 'd' = return R
     keyDispatch _   = Nothing
 
+data GraphicGrid = GraphicGrid
+    { gHeight :: Int
+    , gWidth  :: Int
+    , blocks  :: [(Float, Float)] -- topleft points of blocks
+    , ggen    :: StdGen
+    }
+
+graphicGrid :: Int -> Int -> StdGen -> GraphicGrid
+graphicGrid h w = GraphicGrid h w bs where bs = [ (fromIntegral r, fromIntegral c) | r <- [0, h `div` 4 .. h - 1], c <- [0, w `div` 4 .. w - 1] ]
+
+gridPicture :: GraphicGrid -> Picture
+gridPicture gg = pictures (blockFrame <$> blocks gg)
+  where
+    blockFrame (r, c) = line [(r, c), (r + perHeight, c), (r + perHeight, c + perWidth), (r, c + perWidth), (r, c)]
+    perHeight = fromIntegral (gHeight gg `div` 4)
+    perWidth  = fromIntegral (gWidth gg `div` 4)
+
+renderGrid :: GraphicGrid -> Grid -> Picture
+renderGrid gg g = pictures (debuggingAnchor : gridPicture gg : numbers)
+  where
+    numbers   = app . bimap (uncurry translate . bimap ((perHeight *) . fromIntegral) ((perWidth *) . fromIntegral)) (text . show) <$> assocs (unGrid g)
+    perHeight = fromIntegral (gHeight gg `div` 4)
+    perWidth  = fromIntegral (gWidth gg `div` 4)
+
+debuggingAnchor :: Picture
+debuggingAnchor = pictures [line [(0, 0), (-100, 0)], line [(0, 0), (0, -100)]]
+
+data GameExit = GameExit
+    deriving Show
+
+instance Exception GameExit
+
 main :: IO ()
-main = newGame
+main = do
+    gen <- getStdGen
+    catch (play (InWindow "Nice Window" (800, 600) (100, 100)) white 30 (graphicGrid 800 600 gen, emptyGrid) (translate (-400) (-300) . uncurry renderGrid) handleEvent (const id))
+        $ \case
+              GameExit -> return ()
+              e        -> throw e
+  where
+    handleEvent e world = case e of
+        EventKey (Char c) Down _ _ -> dispatch c world
+        _                          -> world
+    dispatch key (gg, g) = case key of
+        'q' -> throw GameExit
+        c | c `elem` "wsad" -> let (g', gen) = generateNum (g `move` dispatchDirection c, ggen gg) in (gg { ggen = gen }, g')
+          | otherwise       -> (gg, g)
+    dispatchDirection 'w' = U
+    dispatchDirection 's' = D
+    dispatchDirection 'a' = L
+    dispatchDirection 'd' = R
